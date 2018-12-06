4368eb57cd487d0493de1b4d003fd8b012b3a0d0
Edwin Cloud <ecloud412@gmail.com>
2018-12-05 16:05:14 -0800

    Finished

---
+++ tests/sample.test.js
@@ -0,0 +1,17 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const should = chai.should();
+
+function greaterThanFive(num) {
+  if (num > 5) return true;
+  return false;
+}
+
+describe('Example Sinon Stub', () => {
+  it('should pass', (done) => {
+      const greaterThanFive = sinon.stub().returns('something');
+      greaterThanFive(0).should.eql('something');
+      greaterThanFive(0).should.not.eql(false);
+      done();
+  });
+});
